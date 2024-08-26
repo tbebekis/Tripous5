430914309616add6ce0eb93cf6313714a96595db
Theodoros Bebekis <tbebekis@antyxsoft.com>
2024-08-26 14:48:59 +0300

    added AppSettingsBase class

---
+++ Tripous/Core/AppSettingsBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tripous
+{
+
+    /// <summary>
+    /// Base class for application settings.
+    /// <para>Watches for changes in the settings file and reloads this instance.</para>
+    /// </summary>
+    public class AppSettingsBase
+    {
+        const string SFileName = "AppSettings.json";
+
+        /* protected */
+        protected string Folder { get; private set; }
+        protected string FileName { get; private set; }        
+        protected string FilePath { get; private set; }
+        protected FileSystemWatcher Watcher { get; private set; }
+
+        /// <summary>
+        /// event handler
+        /// </summary>
+        protected void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string S = e.FullPath.ToLower();
+            string S2 = FilePath.ToLower();
+
+            if (S == S2)
+                Load();
+        }
+
+
+        /* construction */
+        /// <summary>
+        /// Private constructor
+        /// </summary>
+        AppSettingsBase()
+        {
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        protected AppSettingsBase(string sFolder = "", string sFileName = "")
+        {
+            this.Folder = sFolder;
+            this.FileName = sFileName;
+
+            
+            if (string.IsNullOrWhiteSpace(Folder))
+                Folder = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+
+            if (string.IsNullOrWhiteSpace(FileName))
+                FileName = SFileName;
+
+            FilePath = Path.Combine(Folder, FileName);
+
+            if (!File.Exists(FilePath))
+            {
+                if (!Directory.Exists(Folder))
+                    Directory.CreateDirectory(Folder);
+                Save();
+            }
+
+
+            Watcher = new FileSystemWatcher(Folder);
+            Watcher.NotifyFilter = NotifyFilters.LastWrite;
+            Watcher.Filter = FileName;
+            Watcher.EnableRaisingEvents = true;
+            Watcher.Changed += Watcher_Changed;
+        }
+
+
+        /// <summary>
+        /// Loads this instance's properties from the disk file
+        /// </summary>
+        public virtual void Load()
+        {
+            Json.LoadFromFile(this, FilePath);
+        }
+        /// <summary>
+        /// Saves this instance to a disk file
+        /// </summary>
+        public virtual void Save()
+        {
+            Json.SaveToFile(this, FilePath);
+        }
+    }
+}
